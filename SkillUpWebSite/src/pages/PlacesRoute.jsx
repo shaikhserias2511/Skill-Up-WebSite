23afdd9c7992aae08d2ebb527447851d692a359d
shaikhserias2511 <azruddin20oct1999@gmail.com>
2024-09-06 11:09:27 +0530

    Skill-Up-Web

---
+++ SkillUpWebSite/src/pages/PlacesRoute.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Places from "../components/Places/Places";
+
+const PlacesRoute = () => {
+  return (
+    <>
+      <div className="pt-14">
+        <Places />
+      </div>
+    </>
+  );
+};
+
+export default PlacesRoute;
